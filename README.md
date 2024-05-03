51503f43e388ca4e366bf1dbe639e185ef65c89f
josers77j <josers77.1@gmail.com>
2024-05-03 00:18:10 -0600

    feat: roles and permission in progress

--- README.md
+++ README.md
@@ -22,10 +22,10 @@ The Laravel framework is open-sourced software licensed under the [MIT license](
 - wsl hostname -I
 
 ## ALIAS EN ZSH
-alias sail='./vendor/bin/sail'
+- alias sail='./vendor/bin/sail'
 
 
-# Asegúrate de que tengas una clave SSH pública y privada configurada en tu sistema. Por lo general, se encuentran en ~/.ssh/id_rsa (clave privada) y ~/.ssh/id_rsa.pub (clave pública).
+## Asegúrate de que tengas una clave SSH pública y privada configurada en tu sistema. Por lo general, se encuentran en ~/.ssh/id_rsa (clave privada) y ~/.ssh/id_rsa.pub (clave pública).
 ## Inicia el agente SSH:
 Abre una terminal y ejecuta el siguiente comando para iniciar el agente SSH:
 - eval "$(ssh-agent -s)"
@@ -35,6 +35,6 @@ Usa el siguiente comando para agregar tu clave privada al agente SSH:
 ## Configura tu archivo SSH config (opcional pero recomendado):
 - Puedes configurar tu archivo ~/.ssh/config para especificar la clave SSH a usar para ciertos hosts.
 - Abre o crea el archivo ~/.ssh/config y agrega la configuración para tu host de GitHub:
-
--Host github.com
+---
+- Host github.com
 -  IdentityFile ~/.ssh/id_rsa
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Laravel\Jetstream\Http\Controllers\Inertia\UserProfileController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -13,7 +16,21 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware([
+Route::get('/dashboard', function (){
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->group(function (){
+    Route::get('/profile', [UserProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [UserProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [UserProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function (){
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
+/* Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
@@ -22,3 +39,4 @@ Route::middleware([
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
+ */
